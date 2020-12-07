5b35b90bca66e7b102307fc64521c3b4a7658e17
Shantanu1099 <38537606+Shantanu1099@users.noreply.github.com>
2020-12-08 01:04:34 +0530

    Codeforces Easy Optimised

--- CF_boyORgirl.cpp
+++ CF_boyORgirl.cpp
@@ -2,23 +2,20 @@
 using namespace std;
 int main() {
 
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// #endif
 
 	string name; cin >> name;
 	int size = name.length();
 	int count = 0;
+	sort(name.begin(), name.end());
 	for (int i = 0; i < size; i++) {
-		for (int j = i + 1 ; j <= size; j++) {
-			if (name[i] == name[j])
-				count++;
-			else
-				continue;
+		if (name[i] != name[i + 1]) {
+			count++;
 		}
 	}
-	count = size - count;
 	if (count % 2 == 0 && count > 0)
 		cout << "CHAT WITH HER!";
 	else
