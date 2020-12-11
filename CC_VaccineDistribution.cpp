66051c2adbdc95c7adfd761d25621ece62861943
Shantanu1099 <38537606+Shantanu1099@users.noreply.github.com>
2020-12-11 21:39:16 +0530

    Updated

--- CC_VaccineDistribution.cpp
+++ CC_VaccineDistribution.cpp
@@ -1,25 +1,8 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+#include<iostream>
+#include<math.h>
 using namespace std;
-#define ll long long
-
-void dIsGreater(int n, int d, int arr[]) {
-	ll countCritical = 0;
-	ll dayCr = 0;
-	ll dayNormal = 0;
-	ll countNormal = 0;
-	ll totalDays = 0;
-	for (int i = 0; i < n; i++) {
-		if (arr[i] >= 80 || arr[i] <= 9) {
-			countCritical++;
-		}
-	}
-	dayCr = ceil(countCritical / d);
-	remPeople = n - countCritical;
-	dayNormal = ceil(remPeople / d);
-	totalDays = dayNormal + dayCr;
-	cout << totalDays << endl;
-
-}
+// #define	ll long long
 
 int main() {
 
@@ -29,111 +12,33 @@ int main() {
 #endif
 
 	int t; cin >> t;
-	ll n;
-	ll d;
+	int n;
+	int d;
 	while (t--) {
 		cin >> n;
 		cin >> d;
 		int arr[n] ;
-		// ll countCritical = 0;
-		// ll dayCr = 0;
-		// ll dayNormal = 0;
-		// ll remPeople = 0;
-		// ll totalDays = 0;
 		for (int i = 0; i < n; i++) {
 			cin >> arr[i];
 		}
-		if (d == 1) {
-			cout << n << endl;
+		int countCritical = 0;
+		int dayCr = 0;
+		int dayNormal = 0;
+		int countNormal = 0;
+		int totalDays = 0;
+		for (int  i = 0; i < n; i++) {
+			if (arr[i] >= 80 || arr[i] <= 9) {
+				countCritical++;
+			}
+			else {
+				countNormal++;
+			}
 		}
-		else
-			dIsGreater(n, d, arr);
-		// for (int i = 0; i < n; i++) {
-		// 	if (arr[i] >= 80 || arr[i] <= 9) {
-		// 		countCritical++;
-		// 	}
-		// 	else {
-		// 		continue;
-		// 	}
-		// }
-		// dayCr = ceil(countCritical / d);
-		// remPeople = n - countCritical;
-		// dayNormal = ceil(remPeople / d);
-		// totalDays = dayNormal + dayCr;
-		// cout << totalDays << endl;
+		dayCr = ceil(countCritical / d);
+		dayNormal = ceil(countNormal / d);
+		totalDays = dayNormal + dayCr;
+		cout << totalDays << endl;
+		cout << dayNormal << endl;
 	}
-
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int main() {
-
-// #ifndef ONLINE_JUDGE
-// 	freopen("input.txt", "r", stdin);
-// 	freopen("output.txt", "w", stdout);
-// #endif
-
-// 	int t; cin >> t;
-// 	ll n;
-// 	ll d;
-// 	while (t--) {
-// 		cin >> n;
-// 		cin >> d;
-// 		int arr[n] ;
-// 		ll countCritical = 0;
-// 		ll dayCr = 0;
-// 		ll dayNormal = 0;
-// 		ll remPeople = 0;
-// 		ll totalDays = 0;
-// 		// if (d == 1) {
-// 		// 	cout << n << endl;
-// 		// }
-// 		if (d > 1) {
-// 			for (int i = 0; i < n; i++) {
-// 				cin >> arr[i];
-// 			}
-// 			for (int i = 0; i < n; i++) {
-// 				if (arr[i] >= 80 || arr[i] <= 9) {
-// 					countCritical++;
-// 				}
-// 			}
-// 			dayCr = ceil(countCritical / d);
-// 			remPeople = n - countCritical;
-// 			dayNormal = ceil(remPeople / d);
-// 			totalDays = dayNormal + dayCr;
-// 			cout << totalDays << endl;
-// 			continue;
-// 		}
-// 		else {
-// 			cout << n << endl;
-// 			continue;
-// 		}
-// 	}
-
-// 	return 0;
-// }
