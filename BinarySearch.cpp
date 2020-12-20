52d38f665e399c0c469f546ab31ef9f0337404c5
Shantanu Singh <sshantanuthakur1099@gmail.com>
2020-12-20 23:54:32 +0530

    binary Search

---
+++ BinarySearch.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lli long long int
+#define vi vector<int>
+#define forinput for(int i = 0; i<n ; i++){cin>>arr[i];}
+#define foroutput for(int i = 0; i<n; i++){cout<<arr[i]<<" ";}
+#define pb push_back
+#define pob pop_back
+
+int binarySearch(int arr[], int n, int ele){
+	int low = 0;
+	int high = n-1;
+	while(low <= high){
+		int mid = low + (high-low)/2;
+		if(arr[mid] == ele){
+			return mid;
+		}
+		else if(mid > ele){
+			high = mid - 1;
+		}
+		else{
+			low = mid + 1;
+		}
+	}
+	
+		return 1;
+}
+
+int main(){
+	// "Enter the value of test cases you want "
+int t; cin>>t;
+while(t--){
+	// "Enter the size of array "
+	int n; cin>>n;
+	int arr[n];
+	// "Enter the array with n size separated by a single space "
+	forinput
+	// "Enter tHe Element you wanna Search "
+	int ele; cin>>ele;
+	cout<<binarySearch(arr, n, ele)<<endl;
+}
+					
+return 0;
+}
