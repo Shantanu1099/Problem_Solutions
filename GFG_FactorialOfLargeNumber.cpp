0b70bee2ea938a1a1ef7ebe00f309a47b679ffad
Shantanu Singh <sshantanuthakur1099@gmail.com>
2021-01-03 22:29:16 +0530

    GFG problem

---
+++ GFG_FactorialOfLargeNumber.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lli long long int
+#define vi vector<int>
+#define forinput for(int i = 0; i<n ; i++){cin>>arr[i];}
+#define foroutput for(int i = 0; i<n; i++){cout<<arr[i]<<" ";}
+#define pb push_back
+#define pob pop_back
+
+int factori(lli n){
+	if(n>1){
+return n*factori(n-1);
+}
+else if(n == 1){
+	return 1;
+}
+else{
+	return 0;
+}
+}
+
+int main(){
+int t; cin>>t;
+while(t--){
+	lli n; cin>>n;
+	cout<<factori(n)<<endl;
+}				
+return 0;
+}
