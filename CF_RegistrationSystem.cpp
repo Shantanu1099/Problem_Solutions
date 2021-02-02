bafda0e286bfc186d931825d13bf93428d443b4d
Shantanu Singh <sshantanuthakur1099@gmail.com>
2021-02-02 23:53:01 +0530

    Codeforces C

---
+++ CF_RegistrationSystem.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lli long long int
+#define vi vector<int>
+#define forinput for(int i = 0; i<n ; i++){cin>>arr[i];}
+#define foroutput for(int i = 0; i<n; i++){cout<<arr[i]<<" ";}
+#define pb push_back
+#define pob pop_back
+
+
+int main(){
+
+int n; cin>>n;
+vi v;
+for(int i = 0; i < n; i++)
+{
+v.pb(i);
+}
+
+					
+return 0;
+}
