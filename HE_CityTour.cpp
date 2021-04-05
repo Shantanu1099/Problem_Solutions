c79306b831238e74fd66b92682e092e27bb2d877
Shantanu1099 <sshantanuthakur1099@gmail.com>
2021-04-05 23:23:33 +0530

    Hackerearth easy

---
+++ HE_CityTour.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lli long long int
+#define vi vector<int>
+#define forinput for(int i =0;i<n;i++){cin>>arr[i];}
+#define foroutput for(int i = 0;i<n;i++){cout<<arr[i]<<" ";}
+#define pb push_back
+#define pob pop_back
+int main(){
+int t; cin>>t;
+while(t--){
+lli n; cin>>n;
+cout<<n*4<<endl; 
+}	
+
+	return 0;
+}
