3d8b790ad038b420f6b49886c7f85b535f1ff159
Shantanu1099 <sshantanuthakur1099@gmail.com>
2021-04-07 00:52:26 +0530

    HackerEarth Easy

--- HE_PascalTriangle.cpp
+++ HE_PascalTriangle.cpp
@@ -7,21 +7,33 @@ using namespace std;
 #define foroutput for(int i = 0;i<n;i++){cout<<arr[i]<<" ";}
 #define pb push_back
 #define pob pop_back
+const int N = 42;
 int main(){
+int a[N][N];
+a[0][0] = 1;
+for (int i = 1; i < N; ++i)
+{
+	a[i][0] = 1;
+	a[i][i] = 1;
+	for (int j = 1; j < i; ++j)
+	{
+		a[i][j] = a[i-1][j-1] + a[i-1][j];
+	}
+}
 int t; cin>>t;
 while( t-- ){
 	int n; cin>>n;
-	int a[n][n];
-	a[0][0] = 1;
-	for (int i = 1; i < n; ++i)
-	{
-		a[i][0] = 1;
-		a[i][i] = 1;
-		for (int j = 1; j < i; ++j)
-		{
-			a[i][j] = a[i-1][j-1] + a[i-1][j];
-		}
-	}
+	// int a[n][n];
+	// a[0][0] = 1;
+	// for (int i = 1; i < n; ++i)
+	// {
+	// 	a[i][0] = 1;
+	// 	a[i][i] = 1;
+	// 	for (int j = 1; j < i; ++j)
+	// 	{
+	// 		a[i][j] = a[i-1][j-1] + a[i-1][j];
+	// 	}
+	// }
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j <= i; ++j)
