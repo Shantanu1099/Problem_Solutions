fa48d028a982e740efafa5d25c4cc22d7461d8a6
Shantanu1099 <sshantanuthakur1099@gmail.com>
2021-04-26 01:06:38 +0530

    HackerEarth Hard Array Manipulation

--- test.cpp
+++ test.cpp
@@ -7,9 +7,17 @@ using namespace std;
 #define foroutput for(int i = 0;i<n;i++){cout<<arr[i]<<" ";}
 #define pb push_back
 #define pob pop_back
-// const int N = 1e8;
-// int arr[N];
+const int N = 1e8;
+int arr[N];
 int main(){
+for (int i = 0; i < N; ++i)
+{
+	arr[i] = i;
+}
+cout<<arr[N-1];
+
+
+
 	// arr[0] = 10;
 	// cout<<arr[0]<<endl;
 	// bool a = 0;
@@ -33,10 +41,10 @@ int main(){
 	// cout<<(int)(l_n[l_n.size()-1]-'0')<<endl;
 	// cout<<(int)'0'<<endl;
 
-int a; cin>>a;
-int b; cin>>b;
-int smallest = (a-b)>(b-a)? b:a;
-cout<<smallest<<endl;
+// int a; cin>>a;
+// int b; cin>>b;
+// int smallest = (a-b)>(b-a)? b:a;
+// cout<<smallest<<endl;
 
 	return 0;
 }
