84d6eabb6f94150be6051dffc7ce14883f1bf9fa
Shantanu1099 <sshantanuthakur1099@gmail.com>
2021-05-07 03:44:08 +0530

    STL ITERATORS & RANGE BASED LOOPS USING AUTO Keyword

--- STL.cpp
+++ STL.cpp
@@ -9,8 +9,20 @@ using namespace std;
 #define pob pop_back
 
 
+void vecVecOp(vector< vector< int> > & v){
+	cout << "Size:" << v.size() << endl;
+	for (int i = 0; i < v.size(); ++i)
+	{
+		cout << "Size: " << v[i].size() << endl;
+		for(int j = 0 ; j < v[i].size(); ++j){
+			cout << v[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
 void vecPairOP(vector<pair<int, int>>& a){
-	cout << "Size: " << a.size() <<endl;
+	cout << "Size: " << a.size() << endl;
 	for(int i = 0; i < a.size(); ++i){
 	cout << a[i].first << " " << a[i].second << endl;
 }
@@ -39,26 +51,51 @@ int main(){
 	// Nested vectors
 	// VECTOR of Pairs
 	// vector<pair< int , int > > v = {{2, 3}, {5, 6}, {88, 44}}; Assigning values
-	vector<pair< int, int >> v;
-	int n; cin>> n;
-	for(int i = 0; i < n ; ++i ){ 
-		int x, y; cin >> x >> y;
-		v.pb({x, y});
-	}
-	vecPairOP(v);
+	// vector<pair< int, int >> v;
+	// int n; cin>> n;
+	// for(int i = 0; i < n ; ++i ){ 
+	// 	int x, y; cin >> x >> y;
+	// 	v.pb({x, y});
+	// }
+	// vecPairOP(v);
 
 
 	// 1 - Array of vectors
 	// int n; cin>>n;
-	// vector<int> v2[n];
+	// vector<pair<int, int>> v2[n];
 	// for(int i = 0; i < n; ++i ){
 	// 	int s; cin >> s;
 	// 	for(int j = 0; j < s; ++j){
-	// 		int ip ; cin>>ip;
-	// 		v2[i].pb(ip);
+	// 		int ip, ip2 ; cin >> ip >> ip2 ;
+	// 		v2[i].pb({ip, ip2});
 	// 	}
-	// vecOp(v2[i]);
+	// vecPairOP(v2[i]);
 	// }
+
+
+	// Vector of vectors
+	int N; cin >> N;
+	vector< vector<int> >  v;
+	for(int i = 0; i < N; ++i){
+		int n; cin >> n;
+		vector<int> temp; 
+		// Either use this or push an empty vector inside the main vector
+		// then push values inside this empty vector using element indexing of main vector
+		// v.pb(vector<int> ());
+		for(int j = 0; j < n; ++j){
+			int x; cin >> x;
+			temp.pb(x);
+			// v[i].pb(x);
+		}
+		v.pb(temp);
+	}
+	vecVecOp(v);
+	v.pb(vector<int> ());
+	vecVecOp(v);
+	cout << v[0][2];
+
+
+
 
 	// vector<int> v;
 	// v.pb(88);
@@ -80,5 +117,5 @@ int main(){
 	// v3.pb(101);
 	// vecOp(v3);
 	// vecOp(v);
-	// return 0;
+	return 0;
 }
